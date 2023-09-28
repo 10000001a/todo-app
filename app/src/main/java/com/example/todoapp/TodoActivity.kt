fdb35a34d61ea88c9a28909d7f7128381c0fe73f
10000001a <pgdn06288888@gmail.com>
2023-09-29 04:24:19 +0900

    feat: Add todos to activity with addView().

--- app/src/main/java/com/example/todoapp/TodoActivity.kt
+++ app/src/main/java/com/example/todoapp/TodoActivity.kt
@@ -1,11 +1,25 @@
 package com.example.todoapp
 
 import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class TodoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_todo)
+
+        val container = findViewById<LinearLayout>(R.id.todo_container)
+
+        for (i in 0 until 20) {
+            val todoItemView =
+                this@TodoActivity.layoutInflater.inflate(R.layout.todo_item, null)
+
+            todoItemView.findViewById<TextView>(R.id.todo_name)
+                .also { it.text = "" + i + "번 할일" }
+
+            container.addView(todoItemView)
+        }
     }
 }
