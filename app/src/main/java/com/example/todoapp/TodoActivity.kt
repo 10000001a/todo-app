13fd695d8dff1a914f1b384a325f003dd72326f6
10000001a <pgdn06288888@gmail.com>
2023-10-04 00:30:52 +0900

    ref: Refactor activity.

--- app/src/main/java/com/example/todoapp/TodoActivity.kt
+++ app/src/main/java/com/example/todoapp/TodoActivity.kt
@@ -1,7 +1,6 @@
 package com.example.todoapp
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,48 +8,42 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
 
 class TodoActivity : AppCompatActivity() {
+    private lateinit var todoRecyclerViewLayoutManager: LayoutManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_todo)
 
-        findViewById<RecyclerView>(R.id.todo_recycler_view).also { it ->
-            it.adapter =
-                TodoAdapter(
-                    ArrayList((0 until 40).map { number -> "" + number + "번 할일" }),
-                    this@TodoActivity.layoutInflater
-                )
+        todoRecyclerViewLayoutManager = LinearLayoutManager(this@TodoActivity)
 
-            it.layoutManager = LinearLayoutManager(this@TodoActivity)
+        findViewById<RecyclerView>(R.id.todo_recycler_view).apply {
+            adapter =
+                TodoAdapter(
+                    ArrayList((0 until 40).map { Todo(it.toString(), false) }),
+                )
+            layoutManager = todoRecyclerViewLayoutManager
         }
     }
 }
 
-class TodoAdapter(
-    private val itemList: ArrayList<String>,
-    private val inflater: LayoutInflater
-) : RecyclerView.Adapter<TodoAdapter.TodoViewHolder>() {
+class TodoAdapter(private val dataSet: ArrayList<Todo>) :
+    RecyclerView.Adapter<TodoAdapter.ViewHolder>() {
 
-    class TodoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val todoNameView: TextView
+    class ViewHolder(val view: View) : RecyclerView.ViewHolder(view)
 
-        init {
-            todoNameView = itemView.findViewById(R.id.todo_name)
-        }
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.todo_item, viewGroup, false)
+
+        return ViewHolder(view)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
-        Log.d("Test", "viewType$viewType")
-        return TodoViewHolder(inflater.inflate(R.layout.todo_item, parent, false))
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        viewHolder.view.findViewById<TextView>(R.id.todo_name).text = dataSet[position].title
     }
 
-    override fun getItemCount(): Int {
-        return itemList.size
-    }
-
-    override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
-        Log.d("Test", "position: " + position + "  text: " + holder.todoNameView.text.toString())
-        holder.todoNameView.text = itemList[position]
-    }
+    override fun getItemCount() = dataSet.size
 }
