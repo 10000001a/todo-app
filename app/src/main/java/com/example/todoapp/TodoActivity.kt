cd2f4a94fa0a6aa2a327137243e4a6b67b5cea97
10000001a <pgdn06288888@gmail.com>
2023-10-08 07:15:08 +0900

    feat: Add TodoActivity, InputActivity.

--- app/src/main/java/com/example/todoapp/TodoActivity.kt
+++ app/src/main/java/com/example/todoapp/TodoActivity.kt
@@ -1,11 +1,22 @@
 package com.example.todoapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.todoapp.databinding.ActivityTodoBinding
 
 class TodoActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityTodoBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityTodoBinding.inflate(layoutInflater).apply {
+            setContentView(root)
+            view = this@TodoActivity
+        }
+    }
+
+    fun onClickAdd() {
+        startActivity(Intent(this, InputActivity::class.java))
     }
 }
-
